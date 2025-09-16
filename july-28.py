47ab04308adecac2ef74835ee8c6dd27c671d8bb
Akhila Reddy Gade <gadeakhila39@gmail.com>
2025-09-16 15:46:04 +0530

    Add files via upload

---
+++ july-28.py
@@ -0,0 +1,46 @@
+# for roll_no in range(1,31):
+#     print('class 1', 'roll', roll_no)
+# for roll_no in range(1,31):
+#     print('class 2', 'roll', roll_no)
+# for roll_no in range(1,31):
+#       print('class 3', 'roll', roll_no)
+
+### Nested Loops 
+# count=0
+# for class_no in range(1,11):
+#     #print(class_no)
+#    for roll_no in range(1,31):
+#      count+=1
+#      print('class 1',class_no, 'roll', roll_no)  
+# print (count)
+
+# for j in range(1,10):
+#     for i in range(1,21):
+#         print(j,'X',i, '=', j*i)
+
+# for class_no in range(1,11):
+#    for roll_no in range(1,31):
+#      if roll_no % 2==0:
+#       print('class 1',class_no, 'roll', roll_no) 
+
+# for class_no in range(1,11):
+#    for roll_no in range(1,31):
+#        if class_no % 2 ==0:
+#           print('class 1',class_no, 'roll', roll_no) 
+
+# for class_no in range(1,11):
+#    if class_no % 2 ==0:
+#       print(class_no)
+#    for roll_no in range(1,31):
+#        if roll_no % 2 ==0:
+#           print(roll_no)
+#           print('class 1',class_no, 'roll', roll_no) 
+
+# for class_no in range(1,11):      
+#    for roll_no in range(1,31):   (it takes more time for execution becoz the loop will repeat 300 times. Time taking process)
+#       if class_no % 2 ==0:
+#           print('class 1',class_no, 'roll', roll_no) 
+
+# for class_no in range(1,11):
+#     for roll_no in range(1,31):  (here it takes less time for execution becoz the loop will repeat only 10 times)
+#           print('class 1',class_no, 'roll', roll_no) 
